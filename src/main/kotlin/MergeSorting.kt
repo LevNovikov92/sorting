15604c57bda924a85e4861328e9229a6c5d77d9c
lev.novikov <levnovikov92@gmail.com>
2017-10-27 10:00:41 +0800

    Merge sorting

--- src/main/kotlin/MergeSorting.kt
+++ src/main/kotlin/MergeSorting.kt
@@ -1,27 +1,39 @@
-class MergeSorting : Sorting {
+class MergeSorting {
 
-    override fun sort(arr: IntArray): IntArray {
-        val resultArr = IntArray(arr.size)
-        TODO()
-    }
-
-    fun merge(arr: IntArray, l: Int, m: Int, r: Int): IntArray {
-        val result = IntArray(r - l + 1)
-        var i = l
-        var j = m + 1
-        var k = 0
-        while (i <= m || j <= r) {
-            val a = if (i <= m) arr[i] else null
-            val b = if (j <= r) arr[j] else null
-            if ((b == null && a!= null) || a <= b) {
-                result[k] = a
-                i++
-            } else {
+    fun merge(left: IntArray, right: IntArray) : IntArray {
+        val result = IntArray(left.size + right.size)
+        var i = 0
+        var j = 0
+        for (k in 0 until result.size) {
+            val a = if (i < left.size) left[i] else null
+            val b = if (j < right.size) right[j] else null
+            if (a == null && b != null) {
                 result[k] = b
                 j++
+            } else if (a != null && b == null) {
+                result[k] = a
+                i++
+            } else if (a != null && b != null) {
+                if (a <= b) {
+                    result[k] = a
+                    i++
+                } else {
+                    result[k] = b
+                    j++
+                }
+            } else {
+                throw Exception("Exception")
             }
-            k++
         }
         return result
+    }
+
+    fun sort(arr: IntArray): IntArray {
+        return if (arr.size == 1) {
+            arr
+        } else {
+            val m = (arr.size - 1) / 2
+            merge(sort(arr.copyOfRange(0, m + 1)), sort(arr.copyOfRange(m + 1, arr.size)))
+        }
     }
 }
--- src/test/kotlin/MergeSortingTest.kt
+++ src/test/kotlin/MergeSortingTest.kt
@@ -1,14 +1,19 @@
 import org.junit.Assert
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class MergeSortingTest {
+
     @Test
     fun merge() {
         val arr = intArrayOf(1,3,4,9,2,5,6,8)
-        val result = MergeSorting().merge(arr, 0, 3, arr.size - 1)
+        val result = MergeSorting().merge(arr.copyOfRange(0, 4), arr.copyOfRange(4, 8))
         Assert.assertArrayEquals(intArrayOf(1,2,3,4,5,6,8,9), result)
     }
 
+    @Test
+    fun sort() {
+        val arr = intArrayOf(1,3,8,9,2,5,3,6)
+        val result = MergeSorting().sort(arr)
+        Assert.assertArrayEquals(intArrayOf(1,2,3,3,5,6,8,9), result)
+    }
 }
